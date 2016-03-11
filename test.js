540853aef062a7de125acf30fcd4da2dd12bfca4
andrepolischuk <andre.polischuk@gmail.com>
2016-03-11 15:14:18 +0300

    Drop nodejs 0.10 support

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 import test from 'ava';
 import execa from 'execa';
-import Promise from 'pinkie-promise';
-global.Promise = Promise;
 
 test(async t => {
   const {stdout} = await execa('./cli.js');
