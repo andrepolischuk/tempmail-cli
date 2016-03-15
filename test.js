33ccf3e1b23cfaff9a934c9fa7ca099399240d33
andrepolischuk <andre.polischuk@gmail.com>
2016-03-15 15:51:26 +0300

    Improve test

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 import test from 'ava';
 import execa from 'execa';
+let email;
 
-test('generate a new email address', async t => {
+test('generate a first email address', async t => {
+  const {stdout} = await execa('./cli.js');
+  email = stdout;
+  t.true(stdout.indexOf('@') > 0);
+});
+
+test('return a stored email address', async t => {
   const {stdout} = await execa('./cli.js');
   t.true(stdout.indexOf('@') > 0);
+  t.is(stdout, email);
+});
+
+test('re-generate a new email address', async t => {
+  const {stdout} = await execa('./cli.js', ['--create']);
+  t.true(stdout.indexOf('@') > 0);
+  t.not(stdout, email);
 });
 
 test('get messages', async t => {
