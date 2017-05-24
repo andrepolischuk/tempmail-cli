41e67b5e671f7ed8d69bcd260319401f448c78d9
Andrey Polischuk <me@andrepolischuk.com>
2017-05-24 19:46:32 +0300

    Use standard style instead of airbnb

--- cli.js
+++ cli.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 /* eslint-disable strict, no-console */
-'use strict';
-const meow = require('meow');
-const dim = require('chalk').dim;
-const inquirer = require('inquirer');
-const Readable = require('stream').Readable;
-const pager = require('default-pager');
-const TempMail = require('tempmail.js');
-const Configstore = require('configstore');
-const copy = require('to-clipboard').sync;
-const ansi = require('ansi-escapes');
+'use strict'
+const meow = require('meow')
+const dim = require('chalk').dim
+const inquirer = require('inquirer')
+const Readable = require('stream').Readable
+const pager = require('default-pager')
+const TempMail = require('tempmail.js')
+const Configstore = require('configstore')
+const copy = require('to-clipboard').sync
+const ansi = require('ansi-escapes')
 
-
-process.env.PAGER = process.env.PAGER || 'less';
-process.env.LESS = process.env.LESS || 'FRX';
+process.env.PAGER = process.env.PAGER || 'less'
+process.env.LESS = process.env.LESS || 'FRX'
 
 const cli = meow(`
     Usage
@@ -33,97 +32,99 @@ const cli = meow(`
     g: 'get-mail',
     D: 'delete-all'
   }
-});
+})
 
-function getMessages(account, fn) {
+function getMessages (account, fn) {
   account.getMail().then(messages => {
     if (messages.error) {
-      console.log(messages.error);
+      console.log(messages.error)
     } else {
-      fn(...messages);
+      fn(...messages)
     }
-  });
+  })
 }
 
-function cleanupOutput() {
-  process.stdout.write(ansi.cursorPrevLine + ansi.eraseLine);
+function cleanupOutput () {
+  process.stdout.write(ansi.cursorPrevLine + ansi.eraseLine)
 }
 
-function printMessage(message, messages) {
-  const stream = new Readable({ encoding: 'utf8' });
+function printMessage (message, messages) {
+  const stream = new Readable({
+    encoding: 'utf8'
+  })
 
   stream.push(`${message.mail_from}
 ${dim(message.mail_subject)}
 
 ${message.mail_text}
-  `);
+  `)
 
-  stream.push(null);
+  stream.push(null)
   stream.pipe(pager(() => {
-    cleanupOutput();
+    cleanupOutput()
     /* eslint-disable no-use-before-define */
-    listMessages(...messages);
-  }));
+    listMessages(...messages)
+  }))
 }
 
-function generateChoices(messages) {
+function generateChoices (messages) {
   return messages
     .sort((a, b) => b.mail_timestamp - a.mail_timestamp)
     .map(message => ({
       name: `${message.mail_from} ${dim(message.mail_subject)}`,
       value: message
-    }));
+    }))
 }
 
-function listMessages(...messages) {
-  inquirer.prompt([ {
+function listMessages (...messages) {
+  inquirer.prompt([{
     type: 'list',
     name: 'message',
     message: 'Your messages:',
     choices: generateChoices(messages)
-  } ]).then(answer => {
-    printMessage(answer.message, messages);
-  });
+  }]).then(answer => {
+    printMessage(answer.message, messages)
+  })
 }
 
-function deleteMessages(account, message, ...messages) {
+function deleteMessages (account, message, ...messages) {
   if (!message) {
-    console.log('All messages have been deleted');
+    console.log('All messages have been deleted')
   } else {
     account.deleteMessage(message.mail_id).then(() =>
       setTimeout(() => deleteMessages(...messages), 1000)
-    );
+    )
   }
 }
 
-function printAddress(address) {
+function printAddress (address) {
   try {
-    copy(address);
-    console.log(`${address} ${dim('(copied to clipboard)')}`);
+    copy(address)
+    console.log(`${address} ${dim('(copied to clipboard)')}`)
   } catch (err) {
-    console.log(address);
+    console.log(address)
   }
 }
 
-function exitByQ() {
+function exitByQ () {
   process.stdin.on('keypress', (ch, key = {}) => {
-    if (key.name === 'q') process.exit();
-  });
+    if (key.name === 'q') process.exit()
+  })
 }
 
-const options = new Configstore(cli.pkg.name);
-const account = new TempMail();
-const storedAddress = !cli.flags.create && options.get('email');
+const options = new Configstore(cli.pkg.name)
+const account = new TempMail()
+const storedAddress = !cli.flags.create && options.get('email')
 
 account.create(storedAddress).then(() => {
-  options.set('email', account.address);
+  options.set('email', account.address)
 
   if (cli.flags.getMail) {
-    exitByQ();
-    getMessages(account, listMessages);
+    exitByQ()
+    getMessages(account, listMessages)
   } else if (cli.flags.deleteAll) {
-    getMessages(account, deleteMessages.bind(null, account));
+    getMessages(account, deleteMessages.bind(null, account))
   } else {
-    printAddress(account.address);
+    printAddress(account.address)
   }
-});
+})
--- test.js
+++ test.js
@@ -1,27 +1,27 @@
-import test from 'ava';
-import execa from 'execa';
+import test from 'ava'
+import execa from 'execa'
 
-let out;
+let out
 
 test('generate a first email address', async t => {
-  const { stdout } = await execa('./cli.js');
-  out = stdout;
-  t.true(stdout.indexOf('@') > 0);
-});
+  const {stdout} = await execa('./cli.js')
+  out = stdout
+  t.true(stdout.indexOf('@') > 0)
+})
 
 test('return a stored email address', async t => {
-  const { stdout } = await execa('./cli.js');
-  t.true(stdout.indexOf('@') > 0);
-  t.is(stdout, out);
-});
+  const {stdout} = await execa('./cli.js')
+  t.true(stdout.indexOf('@') > 0)
+  t.is(stdout, out)
+})
 
 test('re-generate a new email address', async t => {
-  const { stdout } = await execa('./cli.js', [ '--create' ]);
-  t.true(stdout.indexOf('@') > 0);
-  t.not(stdout, out);
-});
+  const {stdout} = await execa('./cli.js', [ '--create' ])
+  t.true(stdout.indexOf('@') > 0)
+  t.not(stdout, out)
+})
 
 test('get messages', async t => {
-  const { stdout } = await execa('./cli.js', [ '--get-mail' ]);
-  t.is(stdout, 'There are no emails yet');
-});
+  const {stdout} = await execa('./cli.js', [ '--get-mail' ])
+  t.is(stdout, 'There are no emails yet')
+})
